b307b1ea5389e8f25eb48691beba346c5cd1994a
ikeaorb <164377746+ikeaorb@users.noreply.github.com>
2026-09-08 00:35:20 -0500

    more ui stuff

---
+++ IGOONEDUSINGYOURPEN/Assets/Scripts/UI_Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Movement : MonoBehaviour
+{
+    public float moveDistance;
+    public bool xMovement;
+    public bool yMovement;
+    bool inside;
+    bool outside;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
